e40a85f4f6488f279e8537ae03e5ddf47826085a
ahnjaemin <michael1208@naver.com>
2023-06-04 00:58:13 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+
+	cout << "  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\";
+
+	return 0;
+}
