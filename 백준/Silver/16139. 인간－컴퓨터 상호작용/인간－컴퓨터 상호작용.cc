95292a210893a118b6fa1894230b3d4b90244271
안재민 <michael1208@naver.com>
2024-08-29 17:36:59 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 148 ms, Memory: 29660 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -8,25 +8,25 @@ int l, r, q;
 map<char, vector<int> > m;
 
 int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     cin >> S >> q;
 
     int s = S.size();
+
+    for(int i=1;i<=26;i++){
+        m.insert({96+i, vector<int>(1,0)});
+    }
+
     for(int i=0;i<s;i++){
-
-        if(m.find(S[i]) == m.end()){
-            m.insert({S[i], vector<int>(i, 0)});
-            m[S[i]].push_back(1);
-
-            for(int j=i+1;j<s;j++){
-                if(S[j]==S[i]){
-                    m[S[i]].push_back(m[S[i]][j-1]+1);
-                }
-                else{
-                    m[S[i]].push_back(m[S[i]][j-1]);
-                }
-            }
+        
+        for(int j=1;j<=26;j++){
+            m[96+j].push_back(m[96+j].back());
         }
-
+        
+        m[S[i]][i+1]++;
     }
 
     for(int i=1;i<=q;i++){
@@ -37,14 +37,6 @@ int main(){
         // }
         // cout << "\n";
 
-        if(m.find(a) == m.end()){
-            cout << 0 << "\n";
-            continue;
-        }
-
-        if(l==0)
-            cout << m[a][r] << "\n";
-        else 
-            cout << m[a][r] - m[a][l-1] << "\n";
+        cout << m[a][r+1] - m[a][l] << "\n";
     }
 }
