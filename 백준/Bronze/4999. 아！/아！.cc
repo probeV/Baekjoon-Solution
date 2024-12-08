5a5b1c0b5ca499d1b5e1581415652182c7139e5e
안재민 <michael1208@naver.com>
2024-12-08 18:59:31 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	
+	char a[1001];
+	char b[1001];
+	
+	scanf("%s", a);
+	scanf("%s", b);
+	
+	if(strlen(a)>strlen(b)){
+		printf("go");
+	}
+	else if(strlen(a)==strlen(b)){
+		printf("go");
+	}
+	else{
+		printf("no");
+	}
+	return 0;
+}
