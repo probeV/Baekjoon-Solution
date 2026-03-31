acf3e493d60cda972158bd137cfb6e302b5b26ee
안재민 <ahnjaemin1208@gmail.com>
2026-03-31 20:15:57 +0900

    [Gold V] Title: 0 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.cc
+++ 백준/Gold/7490. 0 만들기/0 만들기.cc
@@ -3,98 +3,32 @@
 using namespace std;
 
 int T, N;
-vector<string> result;
+string str;
 
-void Calculate(deque<char> &arr){
-  int next=0;
-
-  vector<int> num;
-  vector<char> cal;
-
-  // parsing
-  for(char a:arr){
-    
-    if(a >= '0' && a <= '9'){
-      if(next!=0){
-        next = next * 10 + (a-'0');
-      }
-      else{
-        next = a-'0';
-      }
+void Solve(int pos, int sum, int last){
+  if(pos==N+1){
+    if(sum + last==0){
+      cout << str << "\n";
     }
-    else if(a == ' '){
-
-    }
-    else{ // 부호
-      num.push_back(next);
-      next=0;
-
-      cal.push_back(a);
-    }
-  }
-  num.push_back(next);
-
-  // for(int a:num) cout << a << ",";
-  // cout << "\n";
-  // for(char a:cal) cout << a << ",";
-  // cout << "\n";
-
-  // calculate
-  int res=num[0]; 
-  for(int i=1;i<num.size();i++){
-    int C = cal[i-1];
-    if(C=='+'){
-      res = res + num[i];
-    }
-    else { // -
-      res = res - num[i];
-    }
-  }
-
-  // cout << res << "\n\n";
-  // for(char a:arr){
-  //   cout << a;
-  // }
-  // cout << "\n";
-  
-  if(res==0){
-    string str(arr.begin(), arr.end());
-    result.push_back(str);
-    // cout << str << "\n";
-  }
-}
-
-void BackTracking(int prev, deque<char> arr){
-  if(prev>N-1){
-    Calculate(arr);
     return;
   }
 
-  int curr=prev+1;
 
-  // for(char a:arr){
-  //   cout << a;
-  // }
-  // cout << "\n";
-
-  // + 
-  arr.push_back('+');
-  arr.push_back(curr+'0');
-  BackTracking(curr, arr);
-  arr.pop_back(); arr.pop_back();
-
-  // -
-  arr.push_back('-');
-  arr.push_back(curr+'0');
-  BackTracking(curr, arr);
-  arr.pop_back(); arr.pop_back();
-
-  // 이어 붙이기
-  arr.push_back(' ');
-  arr.push_back(curr+'0');
-  BackTracking(curr, arr);
-  arr.pop_back();
+  // 붙이기
+  str+=' '; str += ('0'+pos);
+  int newLast = (last>=0) ? last*10 + pos : last*10-pos; 
+  Solve(pos+1, sum, newLast);
+  str.pop_back(); str.pop_back();
   
+  // 덧셈
+  str += '+'; str += ('0' + pos);
+  Solve(pos + 1, sum + last, pos);
+  str.pop_back(); str.pop_back();
+
+  // 뺄셈
+  str += '-'; str += ('0' + pos);
+  Solve(pos + 1, sum + last, -  pos);
+  str.pop_back(); str.pop_back();
 }
 
 int main(){
@@ -105,18 +39,10 @@ int main(){
   cin >> T;
   for(int i=1;i<=T;i++){
     cin >> N;
-    result.clear();
 
-    deque<char> arr;
-    arr.push_back(1+'0');
-    BackTracking(1, arr);
+    str='1';
+    Solve(2,0,1);
 
-    sort(result.begin(), result.end());
-    
-    for(string s : result) cout << s << "\n";
     cout << "\n";
   }
-
-  
-  
 }
